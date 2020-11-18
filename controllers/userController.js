54a9df1b60783427b8de7dd39ece3400b4d928a7
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-11-18 17:59:23 +0100

    add roles, add getUser, getUser, deleteUser, updateUser possibility

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,7 +43,7 @@ exports.login = async (req, res, next) => {
         if (!validPassword) {
             return next(new Error('Password is not correct'))
         }
-        
+
         const accessToken = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {
             expiresIn: "1d"
         });
@@ -54,5 +54,53 @@ exports.login = async (req, res, next) => {
         })
     } catch (error) {
         next(error);
+    }
+}
+
+exports.getUsers = async (req, res, next) => {
+    const users = await User.find({});
+    res.status(200).json({
+        data: users
+    });
+}
+
+exports.getUser = async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        const user = await User.findById(userId);
+        if (!user) return next(new Error('User does not exist'));
+        res.status(200).json({
+            data: user
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.updateUser = async (req, res, next) => {
+    try {
+        const update = req.body
+        const userId = req.params.userId;
+        await User.findByIdAndUpdate(userId, update);
+        const user = await User.findById(userId)
+        res.status(200).json({
+            data: user,
+            message: 'User has been updated'
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.deleteUser = async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        await User.findByIdAndDelete(userId);
+        res.status(200).json({
+            data: null,
+            message: 'User has been deleted'
+        });
+    } catch (error) {
+        next(error)
     }
 }
