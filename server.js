30d068a1cde1946bb52e43b374117ab12bd78976
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-11-18 18:09:18 +0100

    add more middlewares, add settings to start server

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const express = require("express")
+const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const path = require('path')
+const User = require('./models/userModel')
+const routes = require('./routes/route.js');
 
+require("dotenv").config({
+    path: path.join(__dirname, "../.env")
+});
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
     .connect('mongodb://localhost:27017/SocialNetworkDB')
@@ -16,8 +22,20 @@ mongoose
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(async (req, res, next) => {
+    if (req.headers["x-access-token"]) {
+        const accessToken = req.headers["x-access-token"];
+        const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET);
+        // Check if token has expired
+        if (exp < Date.now().valueOf() / 1000) {
+            return res.status(401).json({ error: "JWT token has expired, please login to obtain a new one" });
+        }
+        res.locals.loggedInUser = await User.findById(userId); next();
+    } else {
+        next();
+    }
+});
 
-app.use('/', routes);
-app.listen(PORT, () => {
+app.use('/', routes); app.listen(PORT, () => {
     console.log('Server is listening on Port:', PORT)
 })
